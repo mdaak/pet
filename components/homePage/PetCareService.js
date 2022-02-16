96c7a033fb1c3d5b7064a1d1ff9f5097005ace0a
mdaak <md.aak3@gmail.com>
2022-02-16 18:09:31 +0600

    2-16

--- components/homePage/PetCareService.js
+++ components/homePage/PetCareService.js
@@ -1,91 +1,113 @@
 import React from "react";
+import { A11y, Autoplay, Navigation, Pagination, Scrollbar } from "swiper";
 import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import { Swiper, SwiperSlide } from "swiper/react";
 import styles from "../../styles/home/PetCareService.module.css";
 
+const swipe = [];
+
+
 const PetCareService = () => {
-    const sliderData=[
-        {
-            id:'1',
-            image:'https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xservices1.jpg.pagespeed.ic.YC-PMYqM1j.webp',
-            name:'pet Training',
-            para:'Far far away, behind the word mountains, far '
-        }
-        ,
-        {
-            id:'2',
-            image:'https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xservices2.jpg.pagespeed.ic.JOkVat6AEA.webp',
-            name:'pet Training',
-            para:'Far far away, behind the word mountains, far '
-        }
-        ,
-        {
-            id:'3',
-            image:'https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xservices3.jpg.pagespeed.ic.1hRXOJt85o.webp',
-            name:'pet Training',
-            para:'Far far away, behind the word mountains, far '
-        }
-        ,
-        {
-            id:'4',
-            image:'https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xservices1.jpg.pagespeed.ic.YC-PMYqM1j.webp',
-            name:'pet Training',
-            para:'Far far away, behind the word mountains, far '
-        }
-        ,
-        {
-            id:'5',
-            image:'https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xservices3.jpg.pagespeed.ic.1hRXOJt85o.webp',
-            name:'pet Training',
-            para:'Far far away, behind the word mountains, far '
-        }
-        ,
-        {
-            id:'6',
-            image:'https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xservices3.jpg.pagespeed.ic.1hRXOJt85o.webp',
-            name:'pet Training',
-            para:'Far far away, behind the word mountains, far '
-        }
-        ,
-        {
-            id:'7',
-            image:'https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xservices3.jpg.pagespeed.ic.1hRXOJt85o.webp',
-            name:'pet Training',
-            para:'Far far away, behind the word mountains, far '
-        }
-    
-    ]
+  const sliderData = [
+    {
+      id: "1",
+      image:
+        "https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xservices1.jpg.pagespeed.ic.YC-PMYqM1j.webp",
+      name: "pet Training",
+      para: "Far far away, behind the word mountains, far ",
+    },
+    {
+      id: "2",
+      image:
+        "https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xservices2.jpg.pagespeed.ic.JOkVat6AEA.webp",
+      name: "pet Training",
+      para: "Far far away, behind the word mountains, far ",
+    },
+    {
+      id: "3",
+      image:
+        "https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xservices3.jpg.pagespeed.ic.1hRXOJt85o.webp",
+      name: "pet Training",
+      para: "Far far away, behind the word mountains, far ",
+    },
+    {
+      id: "4",
+      image:
+        "https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xservices1.jpg.pagespeed.ic.YC-PMYqM1j.webp",
+      name: "pet Training",
+      para: "Far far away, behind the word mountains, far ",
+    },
+    {
+      id: "5",
+      image:
+        "https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xservices3.jpg.pagespeed.ic.1hRXOJt85o.webp",
+      name: "pet Training",
+      para: "Far far away, behind the word mountains, far ",
+    },
+    {
+      id: "6",
+      image:
+        "https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xservices3.jpg.pagespeed.ic.1hRXOJt85o.webp",
+      name: "pet Training",
+      para: "Far far away, behind the word mountains, far ",
+    },
+    {
+      id: "7",
+      image:
+        "https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xservices3.jpg.pagespeed.ic.1hRXOJt85o.webp",
+      name: "pet Training",
+      para: "Far far away, behind the word mountains, far ",
+    },
+  ];
   return (
     <div className={styles.servicDiv}>
-        <Swiper
-          spaceBetween={40}
-          slidesPerView={3}
-          onSlideChange={() => console.log("slide change")}
-          onSwiper={(swiper) => console.log(swiper)}
-          
-          className={styles.serviceSlider}
-        >
-         {
-             sliderData.map((data, i)=>{
-                 return(
-                    <SwiperSlide 
-                        key={i}
-                       className={styles.slide}
-                       style={{backgroundImage:`url(${data.image})`}}
-                       >
-                           <div className={styles.cardText}>
-                               <h2>{data.name}</h2>
-                               <p>{data.para}</p>
-                           </div>
-                       
-                      </SwiperSlide>
-
-                     
-                 )
-             })
-         }
-         
-        </Swiper>
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay, Scrollbar, A11y]}
+        navigation
+        autoplay = {true}
+        spaceBetween={40}
+        slidesPerView={3}
+        pagination={{ clickable: true }}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+        className={styles.serviceSlider}
+        breakpoints={{
+          "340px": {
+            slidesPerView: 1,
+            spaceBetween: 0,
+          },
+          "768px": {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          "1280px": {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          "1366px": {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
+      >
+        {sliderData.map((data, i) => {
+          return (
+            <SwiperSlide
+              key={i}
+              className={styles.slide}
+              style={{ backgroundImage: `url(${data.image})` }}
+            >
+              <div className={styles.cardText}>
+                <h2>{data.name}</h2>
+                <p>{data.para}</p>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </div>
   );
 };
--- components/homePage/Swipers.js
+++ components/homePage/Swipers.js
@@ -30,74 +30,28 @@ const Swipers = () => {
           onSwiper={(swiper) => console.log(swiper)}
           onSlideChange={() => console.log("slide change")}
         >
-          <SwiperSlide className={styles.swiperSlider}>
-            <Image
-              src="https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xtestimonial.png.pagespeed.ic.9W67cSLtTw.webp"
-              alt=""
-              height="100px"
-              width="100px"
-            />
-            <p>
-              <b>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industrys standard dummy text
-                ever since the 1500s, when an unknown printer took a galley
-              </b>
-            </p>
-            <p>Pete Sariya</p>
-            <p>Happy dog owner</p>
-          </SwiperSlide>
-          <SwiperSlide className={styles.swiperSlider}>
-            <Image
-              src="https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xtestimonial.png.pagespeed.ic.9W67cSLtTw.webp"
-              alt=""
-              height="100px"
-              width="100px"
-            />
-            <p>
-              <b>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industrys standard dummy text
-                ever since the 1500s, when an unknown printer took a galley
-              </b>
-            </p>
-            <p>Pete Sariya</p>
-            <p>Happy dog owner</p>
-          </SwiperSlide>
-          <SwiperSlide className={styles.swiperSlider}>
-            <Image
-              src="https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xtestimonial.png.pagespeed.ic.9W67cSLtTw.webp"
-              alt=""
-              height="100px"
-              width="100px"
-            />
-            <p>
-              <b>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industrys standard dummy text
-                ever since the 1500s, when an unknown printer took a galley
-              </b>
-            </p>
-            <p>Pete Sariya</p>
-            <p>Happy dog owner</p>
-          </SwiperSlide>
-          <SwiperSlide className={styles.swiperSlider}>
-            <Image
-              src="https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xtestimonial.png.pagespeed.ic.9W67cSLtTw.webp"
-              alt=""
-              height="100px"
-              width="100px"
-            />
-            <p>
-              <b>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industrys standard dummy text
-                ever since the 1500s, when an unknown printer took a galley
-              </b>
-            </p>
-            <p>Pete Sariya</p>
-            <p>Happy dog owner</p>
-          </SwiperSlide>
+          
+            <SwiperSlide className={styles.swiperSlider}>
+              <div>
+              <Image
+                src="https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xtestimonial.png.pagespeed.ic.9W67cSLtTw.webp"
+                alt=""
+                height="100px"
+                width="100px"
+              />
+              <p>
+                <b>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industrys
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley
+                </b>
+              </p>
+              <p>Pete Sariya</p>
+              <p>Happy dog owner</p>
+              </div>
+            </SwiperSlide>
+        
         </Swiper>
       </div>
     </div>
