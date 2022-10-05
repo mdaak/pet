be3a321bc9fbd5cb4f2f918bad9bb34da4563088
mdaak <md.aak3@gamil.com>
2022-10-06 02:18:48 +0600

    hero section

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -4,11 +4,17 @@ import styles from '../styles/hero_section.module.css'
 function HeroSection() {
     return (
         <div className={styles.container}>
-            <div>
-                <Image src='https://preview.colorlib.com/theme/thepetcare/assets/img/hero/h1_hero1.png.webp' alt='dog image' layout='fill' />
+            <div className={styles.context}>
+                <div className={styles.comtainer_image}>
+                    <Image  src='https://preview.colorlib.com/theme/thepetcare/assets/img/hero/h1_hero1.png.webp' alt='dog image'
+                    // layout='fill' 
+                    width='100%'
+                    height='100%'
+                    />
+                </div>
+
                 <div className={styles.hero_text}>
-                    <h1
-                    >Saving Animals Changing Live</h1>
+                    <h1>Saving Animals Changing Live</h1>
                     <p>Far far away, behind the word mountains, far from the countries vokalia.</p>
                     <button>Out Service</button>
                 </div>
