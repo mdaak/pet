63f610f60101bebb5b98faa01839b58b89cc26bd
mdaak <md.aak3@gamil.com>
2022-10-06 19:24:32 +0600

    hero secion

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -6,17 +6,28 @@ function HeroSection() {
         <div className={styles.container}>
             <div className={styles.context}>
                 <div className={styles.comtainer_image}>
-                    <Image  src='https://preview.colorlib.com/theme/thepetcare/assets/img/hero/h1_hero1.png.webp' alt='dog image'
-                    // layout='fill' 
-                    width='100%'
-                    height='100%'
+                    <Image src='https://preview.colorlib.com/theme/thepetcare/assets/img/hero/h1_hero1.png.webp' alt='dog image'
+                        layout='fill'
                     />
                 </div>
 
                 <div className={styles.hero_text}>
-                    <h1>Saving Animals Changing Live</h1>
-                    <p>Far far away, behind the word mountains, far from the countries vokalia.</p>
-                    <button>Out Service</button>
+                    <div className={styles.hero_text_heading}>
+                        <h1>Saving Animals <br/> Changing Live</h1>
+                        <p>Far far away, behind the word mountains, far from the <br /> countries vokalia.</p>
+                        <button>Out Service</button>
+                    </div>
+                    <div className={styles.hero_card}>
+                        <div>
+                            <Image src='https://preview.colorlib.com/theme/thepetcare/assets/img/gallery/xwoner-img.jpg.pagespeed.ic.1jiOSH1JZw.webp'  
+                            height='150px'
+                            width='150px'
+                            alt='lady'/>
+                        </div>
+                        <div className={styles.hero_card_text}>
+                            <p>The Kind of Care Your<br/>  Pets Deserve</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
