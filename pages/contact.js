53aa1fd0396de53721ef99ddad26d0f4f5f4181a
mdaak <md.aak3@gamil.com>
2022-10-07 21:50:05 +0600

    hero section mediaquery

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import EmergencyCare from '../components/homePage/EmergencyCare'
 import Layout from '../components/Layout'
 
 function contact() {
@@ -7,6 +8,7 @@ function contact() {
     <div>
         <Layout>
             <HeroSection/>
+            <EmergencyCare />
         </Layout>
     </div>
   )
