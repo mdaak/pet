2028e72c08c03ae47ed7e4700606591b2bb93d85
mdaak <md.aak3@gamil.com>
2022-10-08 20:06:31 +0600

    modify

--- pages/service.js
+++ pages/service.js
@@ -4,12 +4,12 @@ import Layout from '../components/Layout';
 
 function service() {
   const service_hero = {
-    img:'https://images.pexels.com/photos/7291997/pexels-photo-7291997.jpeg?auto=compress&cs=tinysrgb&w=600',
+    img:'https://images.pexels.com/photos/7291997/pexels-photo-7291997.jpeg?auto=compress&cs=tinysrgb&w=1200',
     heading1:'Saving Animals',
     heading2:'Changing Live',
     contant1:'Far far away, behind the word mountains, far from the',
     contant2:'',
-    cardImage:'https://images.pexels.com/photos/5377397/pexels-photo-5377397.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    cardImage:'https://images.pexels.com/photos/5377397/pexels-photo-5377397.jpeg?auto=compress&cs=tinysrgb&w=1260&h=1260&dpr=1',
     cardText:'The Kind of treatment Your'
 }
   return (
